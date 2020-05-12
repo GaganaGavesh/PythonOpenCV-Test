e88b5cf9992d3093d5704f938b61181eb99f4591
GaganaGavesh <gaveshgamage@gmail.com>
2020-05-12 23:54:04 +0530

    added imageStack function

--- chapter7.py
+++ chapter7.py
@@ -2,6 +2,37 @@ import cv2
 import numpy as np
 def empty(a):
     pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 #open cv wala hue eka 360k ma ne eke tynne 0-179 dakwa values 180k witharai
 path = 'Resources/lambo.png'
 cv2.namedWindow("TrackBars")
@@ -29,10 +60,12 @@ while True:
 
     mask = cv2.inRange(imgHSV,lower,upper)
     imgResult = cv2.bitwise_and(img,img,mask=mask)
+    imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
     print(h_min,h_max,s_min,s_max,v_min,v_max)
 
-    cv2.imshow("Lambo Image",img)
-    cv2.imshow("LamboHSV Image",imgHSV)
-    cv2.imshow("LamboMask Image", mask)
-    cv2.imshow("LamboFinal Image", imgResult)
+    # cv2.imshow("Lambo Image",img)
+    # cv2.imshow("LamboHSV Image",imgHSV)
+    # cv2.imshow("LamboMask Image", mask)
+    # cv2.imshow("LamboFinal Image", imgResult)
+    cv2.imshow("StackImages", imgStack)
     cv2.waitKey(1)

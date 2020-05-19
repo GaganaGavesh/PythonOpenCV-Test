81a054d113846b08266f18aefa9852aadaad702c
GaganaGavesh <gaveshgamage@gmail.com>
2020-05-19 18:41:20 +0530

    Completed chapter8,added shape recoginzer part

--- chapter8.py
+++ chapter8.py
@@ -32,14 +32,50 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
+def getContours(img):
+    contours,hierachy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        #print(area)
+        if area>500:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            print(len(approx))
+            objCor = len(approx)
+            x,y,w,h = cv2.boundingRect(approx)
+            aspectRatio = w/float(h)
+            print(aspectRatio)
+
+            if objCor ==3:
+                objectType ="Tringle"
+            elif objCor ==4:
+                aspectRatio = w / float(h)
+                print(aspectRatio)
+                if aspectRatio >0.95 and aspectRatio <1.05:
+                    objectType ="Square"
+                else:
+                    objectType ="Rectrangle"
+            else:
+                objectType ="Circle"
+            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(imgContour,objectType,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (0,0,0),2)
+
+
+
 path = 'Resources/shapes.png'
 img = cv2.imread(path)
+imgContour = img.copy()
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
 imgCanny = cv2.Canny(img,50,50)
 imgBlank = np.zeros_like(img)
-imgStack = stackImages(0.6,([img,imgGray,imgBlur],[imgCanny,imgBlank,imgBlank]))
+getContours(imgCanny)
+imgStack = stackImages(0.6,([img,imgGray,imgBlur],[imgCanny,imgContour,imgBlank]))
 
 # cv2.imshow("Shapes Image",img)
 # cv2.imshow("Shapes ImageGray",imgGray)
